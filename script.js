568451444a4fbf788da87b9f115c68f1c4c9a0c8
shahriarislamratul <153156266+shahriarislamratul@users.noreply.github.com>
2024-06-04 08:44:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector('#btn');
+const password = document.querySelector('#inPutPass');
+const passLengthInput = document.querySelector('#passLength');
+const upperCheckbox = document.querySelector('#upper');
+const lowerCheckbox = document.querySelector('#lower');
+const numberCheckbox = document.querySelector('#number');
+const symbolCheckbox = document.querySelector('#symbol');
+const copyBtn = document.querySelector('#copyBtn');
+
+function generateCode(length = 6) {
+  const upper = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+  const lower = 'qwertyuiopasdfghjklzxcvbnm';
+  const numbers = '0123456789';
+  const symbols = '@#%&*!?$';
+
+  let phrase = '';
+
+  if (upperCheckbox.checked) phrase += upper;
+  if (lowerCheckbox.checked) phrase += lower;
+  if (numberCheckbox.checked) phrase += numbers;
+  if (symbolCheckbox.checked) phrase += symbols;
+
+  if (phrase === '') {
+    return 'Select at least one option';
+  }
+
+  let pass = '';
+  for (let x = 0; x < length; x++) {
+    pass += phrase[Math.floor(Math.random() * phrase.length)];
+  }
+
+  return pass;
+}
+
+btn.addEventListener('click', () => {
+  const length = parseInt(passLengthInput.value);
+  password.value = generateCode(length);
+});
+
+copyBtn.addEventListener('click', () => {
+  password.select();
+  document.execCommand('copy');
+  alert('Password copied to clipboard!');
+});
